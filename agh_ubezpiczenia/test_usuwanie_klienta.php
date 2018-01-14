5ff2a95f164eb9b4740ef74d56034dea4e708bdd
km95 <sz.ka@interia.pl>
2018-01-14 16:59:16 +0100

    Testy

---
+++ agh_ubezpiczenia/test_usuwanie_klienta.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Dodaj ekran</title>
+</head>
+<body>
+	<?php
+			include "funkcje.php";
+			$polaczenie = Polacz();
+				if($polaczenie === false)
+					die("BŁĄD: Nie można połączyć " . mysqli_connect_error());
+				
+				$zapytanie = "DELETE FROM klienci WHERE imie ='Test'";
+				
+				if(mysqli_query($polaczenie, $zapytanie))
+					echo '<font color="#15dc00">'."Usunieto wpis.".'</font>'.'<br><br>';
+				else
+					echo "BŁĄD: Nie można wykonać polecenia $zapytanie. " . mysqli_error($polaczenie);
+
+				mysqli_close($polaczenie);
+
+	?>
+	
+	<form action="test_usuwanie_klienta.php" method="post">
+	<table>
+		<b>Usuwanie:</b><br>
+
+	</table>	
+	</form>
+
+	
+</body>
+</html>
